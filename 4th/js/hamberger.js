b781cd1db42290e70c3d172f9a5df5c199f38bf5
hminn <hmkim0008@gmail.com>
2021-05-06 12:25:01 +0900

    style : 메뉴 버튼 태그 변경
    - 'a' 태그에서 'div' 태그로 변경
    - 포인터 커서 CSS 속성 추가

--- 4th/js/hamberger.js
+++ 4th/js/hamberger.js
@@ -1,9 +1,8 @@
 hamberger = (function () {
   const hamberger = document.getElementById('hamberger');
 
-  const hambergerItem = document.createElement('a');
+  const hambergerItem = document.createElement('div');
   hambergerItem.className = 'hamberger-item';
-  hambergerItem.href = "#";
   hamberger.appendChild(hambergerItem);
   for (var i = 0; i < 3; i++){
     hambergerItem.appendChild(document.createElement('span'));
