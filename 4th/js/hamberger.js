f5dd080bb0491afbd32b1deb3ee8904bba903b42
hminn <hmkim0008@gmail.com>
2021-05-06 12:30:37 +0900

    style : 메뉴창 스타일 속성 수정
    - 불필요한 속성 제거

--- 4th/js/hamberger.js
+++ 4th/js/hamberger.js
@@ -1,13 +1,11 @@
 hamberger = (function () {
   const hamberger = document.getElementById('hamberger');
-
   const hambergerItem = document.createElement('div');
   hambergerItem.className = 'hamberger-item';
   hamberger.appendChild(hambergerItem);
   for (var i = 0; i < 3; i++){
     hambergerItem.appendChild(document.createElement('span'));
   }
-
   hambergerItem.addEventListener('click', (event) => {
     event.currentTarget.classList.toggle('active');
     hamberger.classList.toggle('active');
