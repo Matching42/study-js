0fb4d08d1b3e3b3bb9298fb2da7ec7638af3e24c
snagjaepark <atimangel@naver.com>
2021-05-14 13:27:27 +0900

    sorry

---
+++ 5th/snpark/forbidden_fruit.js
@@ -0,0 +1,17 @@
+var arr;
+
+function gatcha()
+{
+	var i;
+
+	for (i = 0; i < 5; i++)
+	{
+		arr[i] = functio(){return i;}
+	}
+}
+
+getcha();
+for (var index in arr)
+{
+	console.log(arr[index]());
+}
