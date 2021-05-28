afae78c72a9393d1190faa0a4eb2392acfac89e1
from97 <skm1104@daum.net>
2021-05-28 15:31:04 +0900

    seomoon: Dark Mode 위치 좌측 상단으로 이동

--- 6th/seomoon/src/components/DarkMode.js
+++ 6th/seomoon/src/components/DarkMode.js
@@ -1,20 +1,26 @@
-const TEMPLATE = '<input type="checkbox">';
 
 export default class DarkMode {
     constructor({ $target, onClick }) {
+        const $darkModeWrapper = document.createElement('div');
+        this.$darkModeWrapper = $darkModeWrapper;
+        this.$darkModeWrapper.className = 'DarkMode';
+
         const $darkMode = document.createElement("input");
         this.$darkMode = $darkMode;
         this.$darkMode.type = 'checkbox';
         this.$darkMode.id = 'darkmode';
         this.$darkMode.name = 'darkmode';
         
-        $target.appendChild($darkMode);
-        
         const $label = document.createElement("label");
         this.$label = $label;
-        this.$label.for = 'darkmode';
+        this.$label.setAttribute('for', 'darkmode');
         this.$label.innerText = 'Dark Mode';
-        $target.appendChild($label);
+        this.$label.className = '.DarkModeLabel';
+
+        this.$darkModeWrapper.appendChild($darkMode);
+        this.$darkModeWrapper.appendChild($label);
+
+        $target.appendChild($darkModeWrapper);
 
         $darkMode.addEventListener('click', onClick);
     }
